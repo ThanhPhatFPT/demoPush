22b54c6debaaae7a49e77e320e02e1811759f0df
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2024-04-04 23:34:43 +0700

    how to fix?

---
+++ Downloads/demoPush-master/demoPush-master/src/main/java/lad6_1/NewClass.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lad6_1;
+
+/**
+ *
+ * @author votha
+ */
+public class NewClass {
+    private String name;
+    private String age;
+
+    public NewClass() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+    
+}
