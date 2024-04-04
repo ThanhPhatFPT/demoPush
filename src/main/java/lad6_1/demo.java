57891f8f5f39cea8f21034c2599dbe6b56623d50
ThanhPhatFPT <156346544+ThanhPhatFPT@users.noreply.github.com>
2024-04-04 23:28:14 +0700

    first commit

---
+++ src/main/java/lad6_1/demo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lad6_1;
+
+/**
+ *
+ * @author votha
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+        System.out.println("helooo");
+
+    }
+}
